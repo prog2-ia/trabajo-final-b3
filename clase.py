7efd2675e5c5fb05fedf5ea803fa4a633cd63fc9
bmr55 <bmr55@alu.ua.es>
2026-02-27 10:40:08 +0100

    Estructura hecha

--- clase.py
+++
@@ -1,9 +0,0 @@
-class Clase :
-
-    CONSTANTE_DINERO_JUEGO = 123
-
-    def __init__(self , nombre)  :
-        self.nombre = nombre
-
-        # variables
-
---
+++ entidades/colecciones/__init__.py
@@ -0,0 +1,1 @@
+from usurios import usuario ,
