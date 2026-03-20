c135ca9edcc4109c225d0b5a35b897b3a29a2d91
DCP-2007 <dcp57@alu.ua.es>
2026-03-20 10:25:51 +0100

    Getter de piezas y comentarios

--- entidades/colecciones/coleccion.py
+++ entidades/colecciones/coleccion.py
@@ -4,28 +4,28 @@ from ..piezas.figura import Figura
 
 class Coleccion :
     """
-    Clase que representa la colección de piezas de un Usuario.
+        Clase que representa una coleccíon
 
+        ---ATRIBUTOS---
 
-    ---ATRIBUTOS---
+            __id : str
+            __piezas : list[Pieza]
 
-    __id : str
-    __piezas : list[Pieza]
+        ---MÉTODOS---
 
+            agregar_pieza( pieza : Pieza ) -> bool
+                añade una pieza a una colección, si el parámetro dado no
+                es una pieza o está repetido notifica un error
 
-    ---MÉTODOS---
+            eliminar:pieza( pieza : Pieza ) -> bool
+                elimina una pieza de una coleccion, si el parámetro no es
+                pieza o no se encuentra en esa coleccion notifica un error
 
-    agregar_pieza( pieza : Pieza ) -> bool
-        añade una pieza a una colección, si el parámetro dado no
-        es una pieza o está repetido notifica un error
+            __str__():
+                imprimre el id y número de piezas de una colección
 
-    eliminar:pieza( pieza : Pieza ) -> bool
-        elimina una pieza de una coleccion, si el parámetro no es
-        pieza o no se encuentra en esa coleccion notifica un error
-
-    __str__():
-        imprimre el id y número de piezas de una colección
     """
+
     __identificador: int = 0
 
     def __init__(self, piezas: list[Pieza] = None):
@@ -36,7 +36,10 @@ class Coleccion :
             self.__piezas = []
 
         type(self).__identificador += 1
+
         self.__identificador = type(self).__identificador
+
+    #--------------------------PROPIEDADES DE LOS ATRIBUTOS----------------------------------------------
 
     @property
     def identificador(self) -> int:
@@ -45,35 +48,46 @@ class Coleccion :
     @property
     def piezas(self) -> list[Pieza]:
         return  self.__piezas.copy()
+
+    @piezas.setter
+    def piezas(self, value):
+        if value is None:
+            return
+        self.__piezas = value.copy()
+
+    # ------------------------------METODOS----------------------------------------------------------------
     
     def agregar_pieza(self, pieza: Pieza) -> bool:
 
         if not isinstance(pieza, Pieza):
-            #print("Error: El objeto que quieres agregar no es una pieza válida")
             return False
 
         if pieza in self.__piezas:
-            # print("Error: La pieza ya se encuentra en la colección")
             return False
 
         self.__piezas.append(pieza)
+
         return True
 
     def eliminar_pieza(self, pieza: Pieza) -> bool:
+
         if not isinstance(pieza, Pieza):
-            return False # Error: El objeto que quieres eliminar no es una pieza válida
+            return False
 
         if pieza in self.__piezas:
             self.__piezas.remove(pieza)
-            return True # Éxito: La pieza ha sido eliminada correctamente
+            return True
         else:
-            return False # La pieza es válida pero no se encuentra en esta colección
+            return False
 
     def get_figuras(self) -> list[Figura]:
+
         figuras = []
+
         for pieza in self.__piezas:
             if isinstance(pieza,Figura):
                 figuras.append(pieza)
+
         return figuras.copy()
 
     def get_cartas(self) -> list[Carta]:
@@ -82,8 +96,6 @@ class Coleccion :
             if isinstance(pieza,Carta):
                 cartas.append(pieza)
         return cartas.copy()
-
-
 
     def __eq__(self, other) -> bool:
         if isinstance(other, Coleccion):
